eeb41db7c88ef4717895ac25536670577bb83c57
Konstantin Bazhev <k.bazhev@sitesoft.ru>
2023-10-11 04:34:51 +0500

    init

---
+++ resources/js/App.vue
@@ -0,0 +1,70 @@
+<template>
+    <div id="wrapper">
+
+        <!-- sidebar -->
+        <div class="sidebar">
+            <span class="closeButton">&times;</span>
+            <p class="brand-title"><a href="">Alphayo Blog</a></p>
+
+            <div class="side-links">
+                <ul>
+                    <li>
+                        <router-link :to="{name:'Home'}">Home</router-link>
+                    </li>
+                    <li>
+                        <router-link :to="{name:'Blog'}">Blog</router-link>
+                    </li>
+                    <li>
+                        <router-link :to="{name:'About'}">About</router-link>
+                    </li>
+                    <li>
+                        <router-link :to="{name:'Contact'}">Contact</router-link>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- sidebar footer -->
+            <footer class="sidebar-footer">
+                <div>
+                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                    <a href=""><i class="fab fa-instagram"></i></a>
+                    <a href=""><i class="fab fa-twitter"></i></a>
+                </div>
+
+                <small>&copy 2021 Alphayo Blog</small>
+            </footer>
+        </div>
+        <!-- Menu Button -->
+        <div class="menuButton">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+        <!-- main -->
+        <main class="container">
+            <router-view></router-view>
+        </main>
+
+        <!-- Main footer -->
+        <footer class="main-footer">
+            <div>
+                <a href=""><i class="fab fa-facebook-f"></i></a>
+                <a href=""><i class="fab fa-instagram"></i></a>
+                <a href=""><i class="fab fa-twitter"></i></a>
+            </div>
+            <small>&copy 2021 Alphayo Blog</small>
+        </footer>
+    </div>
+
+
+</template>
+
+<script>
+    export default {
+        name: "App.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
